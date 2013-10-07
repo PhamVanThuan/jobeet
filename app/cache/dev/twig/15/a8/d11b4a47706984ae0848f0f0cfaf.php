be21234ece536a8bae51466ea0c8205a0f670a5b
Phạm Văn Thuận <phamthuanpv@gmail.com>
2013-10-07 17:50:54 +0700

    First Commit
    Signed-off-by: Phạm Văn Thuận <phamthuanpv@gmail.com>

---
+++ app/cache/dev/twig/15/a8/d11b4a47706984ae0848f0f0cfaf.php
@@ -0,0 +1,39 @@
+<?php
+
+/* EnsJobeetBundle:Default:index.html.twig */
+class __TwigTemplate_15a8d11b4a47706984ae0848f0f0cfaf extends Twig_Template
+{
+    public function __construct(Twig_Environment $env)
+    {
+        parent::__construct($env);
+
+        $this->parent = false;
+
+        $this->blocks = array(
+        );
+    }
+
+    protected function doDisplay(array $context, array $blocks = array())
+    {
+        // line 1
+        echo "Hello ";
+        echo twig_escape_filter($this->env, (isset($context["name"]) ? $context["name"] : $this->getContext($context, "name")), "html", null, true);
+        echo "!
+";
+    }
+
+    public function getTemplateName()
+    {
+        return "EnsJobeetBundle:Default:index.html.twig";
+    }
+
+    public function isTraitable()
+    {
+        return false;
+    }
+
+    public function getDebugInfo()
+    {
+        return array (  19 => 1,);
+    }
+}
